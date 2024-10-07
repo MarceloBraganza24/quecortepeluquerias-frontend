739d85b45ba93c5e123cb3166e8b837dacb7a702
litocode24 <marcelo_braganza@hotmail.com>
2024-10-07 14:52:52 -0300

    feat: footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,25 +11,25 @@ const Footer = () => {
                 <img className='footerContainer__social-networks__logo-link__logo' src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj53U5Xgk3kFPa85_nVDnhi_026YYTpOvc2huRoqYwyz3cD3nP4Tu8rxYUlKXmQTFymJDI62D2n47AD8pRtXzOUajRf1z9ayWlqBadpesRe_2geUEMq-TB_ZXCir0jFKv6lwUpqe57nT_lSnX_S7n4jowLc152S_R2Qiuxqb8_YXl5AmKQimxsO2ri4As8/s50/qcp-logo-face.png" alt="logo-face" />
                 {
                     !updateShiftModal&&!cancelDayModal&&!myDataModal&&!cancelDaysListModal&&!cancelShiftModal&&!recoverShiftModal&&!updateMyShiftModalMobile&&!updateUserModalMobile&&!createPartnerModalMobile&&!createUserModalMobile&&!createProductModalMobile&&!createProviderModalMobile&&!updateProductModalMobile&&!updatePartnerModalMobile&&!updateProviderModalMobile&&!createShiftModalMobile&&!updateShiftModalMobile&&!updateMyShiftModal&&!updatePartnerModal&&!updateProviderModal&&!updateProductsModal&&!updateUsersModal&&!updatePricesModal&&!deleteTicketModal&&!payMembershipFeeModal?
-                    <a href="/home" className='footerContainer__social-networks__logo-link__link'>MB Peluquerías</a>
+                    <a href="https://www.facebook.com/Quecort" className='footerContainer__social-networks__logo-link__link'>Que Corte</a>
                     :
-                    <a className='footerContainer__social-networks__logo-link__link'>MB Peluquerías</a>
+                    <a className='footerContainer__social-networks__logo-link__link'>Que Corte</a>
                 }
             </div>
         </div>
         <div className='footerContainer__title-copyrigth'>
             <div className='footerContainer__title-copyrigth__title'>
                 <div className='footerContainer__title-copyrigth__title__prop'>
-                    <p className='footerContainer__title-copyrigth__title__prop__spacing'>MB</p>
-                    <p>Peluquerías</p>
+                    <p className='footerContainer__title-copyrigth__title__prop__spacing'>Peluquería</p>
+                    <p>Que Corte</p>
                 </div>
             </div>
             <div className='footerContainer__title-copyrigth__copyrigth'>
             {
                     !updateShiftModal&&!cancelDayModal&&!myDataModal&&!cancelDaysListModal&&!cancelShiftModal&&!recoverShiftModal&&!updateMyShiftModalMobile&&!updateUserModalMobile&&!createPartnerModalMobile&&!createUserModalMobile&&!createProductModalMobile&&!createProviderModalMobile&&!updateProductModalMobile&&!updatePartnerModalMobile&&!updateProviderModalMobile&&!createShiftModalMobile&&!updateShiftModalMobile&&!updateMyShiftModal&&!updatePartnerModal&&!updateProviderModal&&!updateProductsModal&&!updateUsersModal&&!updatePricesModal&&!deleteTicketModal&&!payMembershipFeeModal?
-                    <a href="/home" className='footerContainer__title-copyrigth__copyrigth__prop'>© 2024 - Marcelo Braganza</a>
+                    <a href="/home" className='footerContainer__title-copyrigth__copyrigth__prop'>© 2024 - MB Soluciones Informáticas</a>
                     :
-                    <a className='footerContainer__title-copyrigth__copyrigth__prop'>© 2024 - Marcelo Braganza</a>
+                    <a className='footerContainer__title-copyrigth__copyrigth__prop'>© 2024 - MB Soluciones Informáticas</a>
             }
             </div>
         </div>
@@ -38,9 +38,9 @@ const Footer = () => {
                 <img className='footerContainer__social-networks__logo-link__logo' src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjMjnBxURMsAUtgPWW0aIbooWvkl_OvFFTEv2aSB0AV55ls35auw7bPOnwlcOGeF6IuHP9Gj-u_fHZnyOcrxmUam7z5nSnAcvrKTSgBXzwG_rqUJQAiWOzO8ksMoQd2epp40oQBJ_jqYMdoOk3xWJJ0zs4bv1IA7KfFsgsplEsrA09D-EMDKCoRY8Bz_tU/s50/qcp-logo-insta.png" alt="logo-face" />
                 {
                     !updateShiftModal&&!cancelDayModal&&!myDataModal&&!cancelDaysListModal&&!cancelShiftModal&&!recoverShiftModal&&!updateMyShiftModalMobile&&!updateUserModalMobile&&!createPartnerModalMobile&&!createUserModalMobile&&!createProductModalMobile&&!createProviderModalMobile&&!updateProductModalMobile&&!updatePartnerModalMobile&&!updateProviderModalMobile&&!createShiftModalMobile&&!updateShiftModalMobile&&!updateMyShiftModal&&!updatePartnerModal&&!updateProviderModal&&!updateProductsModal&&!updateUsersModal&&!updatePricesModal&&!deleteTicketModal&&!payMembershipFeeModal?
-                    <a href="/home" className='footerContainer__social-networks__logo-link__link'>MB Peluquerías</a>
+                    <a href="https://www.instagram.com/quecorte__/" className='footerContainer__social-networks__logo-link__link'>quecorte__</a>
                     :
-                    <a className='footerContainer__social-networks__logo-link__link'>MB Peluquerías</a>
+                    <a className='footerContainer__social-networks__logo-link__link'>quecorte__</a>
                 }
             </div>
         </div>
